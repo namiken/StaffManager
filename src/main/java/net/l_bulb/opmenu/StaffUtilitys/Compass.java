168ad13e4b0e5c5fc306a74d8a0a7f07f1336c85
fav <monmon25565@gmail.com>
2017-02-23 08:46:38 +0900

    Compass

--- src/main/java/net/l_bulb/opmenu/StaffUtilitys/Compass.java
+++ src/main/java/net/l_bulb/opmenu/StaffUtilitys/Compass.java
@@ -1,5 +1,45 @@
 package net.l_bulb.opmenu.StaffUtilitys;
 
-public class Compass {
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.util.BlockIterator;
 
+public class Compass implements Listener {
+
+    @EventHandler
+    public void CompassLeftClickEvent(PlayerInteractEvent event) {
+	if(event.getPlayer().hasPermission("staff.staff.tool")) {
+	    return;
+	}
+	if(event.getPlayer().getItemInHand().getType() == Material.COMPASS) {
+	Player player =event.getPlayer();
+	    Block target = getTargetBlock(player);
+	    if (target != null) {
+		player.teleport(target.getLocation());
+		player.sendMessage(ChatColor.GREEN + "[OpMenu] " + ChatColor.YELLOW +  "Teleported.");
+
+	    }
+	}
+    }
+
+    private Block getTargetBlock(Player player) {
+
+	BlockIterator it = new BlockIterator(player, 100);
+
+	while ( it.hasNext()) {
+
+	    Block block = it.next();
+	    if (block.getType() != Material.AIR) {
+		return block;
+	    }
+
+	}
+	player.sendMessage(ChatColor.GREEN + "[OpMenu] " + ChatColor.RED +  "Block Not Found.");
+	return null;
+    }
 }
