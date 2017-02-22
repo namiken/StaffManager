a0699c248a9a2f618fd6821d2e9f071b103e142a
fav <monmon25565@gmail.com>
2017-02-23 07:07:32 +0900

    Command実装部分にItems書くのが邪魔だったのでクラス分け

--- src/main/java/net/l_bulb/opmenu/Commands/StaffCommand.java
+++ src/main/java/net/l_bulb/opmenu/Commands/StaffCommand.java
@@ -1,7 +1,6 @@
 package net.l_bulb.opmenu.Commands;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,9 +8,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.inventory.meta.ItemMeta;
+
+import net.l_bulb.opmenu.StaffUtilitys.StaffItems;
 public class StaffCommand implements Listener,  CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args ) {
@@ -19,53 +17,16 @@ public class StaffCommand implements Listener,  CommandExecutor {
 	}
         if (sender instanceof Player) {
 	    Player player = (Player) sender;
-	    player.sendMessage("player");
+	    onEnableStaffMode(player);
         } else {
             sender.sendMessage("コンソールから実行できません。");
         }
         return true;
     }
-    public static void staffItems(Player player) {
-	PlayerInventory Inventory = player.getInventory();
-	ItemStack ItemStack1 = new ItemStack(Material.COMPASS);
-	ItemStack ItemStack2 = new ItemStack(Material.BOOK);
-	ItemStack ItemStack3 = new ItemStack(Material.RECORD_3);
-	ItemStack ItemStack4 = new ItemStack(Material.INK_SACK);
-	ItemStack ItemStack5 = new ItemStack(Material.SHEARS);
-	ItemStack ItemStack6 = new ItemStack(Material.ICE);
-
-	ItemMeta ItemMeta1 = ItemStack1.getItemMeta();
-	ItemMeta ItemMeta2 = ItemStack2.getItemMeta();
-	ItemMeta ItemMeta3 = ItemStack3.getItemMeta();
-	ItemMeta ItemMeta4 = ItemStack4.getItemMeta();
-	ItemMeta ItemMeta5 = ItemStack5.getItemMeta();
-	ItemMeta ItemMeta6 = ItemStack6.getItemMeta();
-
-	ItemMeta1.setDisplayName(ChatColor.GOLD + "Teleport Compass");
-	ItemMeta2.setDisplayName(ChatColor.GOLD + "Inventory Check");
-	ItemMeta3.setDisplayName(ChatColor.GOLD + "Teleport Random");
-	ItemMeta4.setDisplayName(ChatColor.GOLD + "Staff GUI");
-	ItemMeta5.setDisplayName(ChatColor.GOLD + "Health Check");
-	ItemMeta6.setDisplayName(ChatColor.GOLD + "Freeze Player");
-
-	ItemStack1.setItemMeta(ItemMeta1);
-	ItemStack2.setItemMeta(ItemMeta2);
-	ItemStack3.setItemMeta(ItemMeta3);
-	ItemStack4.setItemMeta(ItemMeta4);
-	ItemStack5.setItemMeta(ItemMeta5);
-	ItemStack6.setItemMeta(ItemMeta6);
-
-	Inventory.setItem(0, ItemStack1);
-	Inventory.setItem(1, ItemStack2);
-	Inventory.setItem(7, ItemStack3);
-	Inventory.setItem(8, ItemStack4);
-	Inventory.setItem(2, ItemStack5);
-	Inventory.setItem(2, ItemStack6);
-    }
 
     public static void onEnableStaffMode(Player player) {
 	player.getInventory().clear();
-	staffItems(player);
+	StaffItems.addstaffItems(player);
 	player.sendMessage(ChatColor.GREEN + "[OpMenu] " + ChatColor.GOLD + "Staff Mode >> Enable!");
     }
 
