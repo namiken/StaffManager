1ef98929ef343e2beab369442916367e0673cdc4
fav <monmon25565@gmail.com>
2017-02-23 23:43:03 +0900

    Chat消すやつ

---
+++ src/main/java/net/l_bulb/opmenu/Commands/ClearChatCommand.java
@@ -0,0 +1,21 @@
+package net.l_bulb.opmenu.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ClearChatCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+	Player player = (Player) sender;
+	if(!player.hasPermission("staff.Clear")) {
+	}
+	int a;
+	for (a = 0; a < 100; a++) {
+	player.sendMessage("                  ");
+	}
+	return true;
+    }
+
+}
