3a518f107319e1e6d2bc2706d8d170ac4750f210
fav <monmon25565@gmail.com>
2017-02-23 23:51:11 +0900

    RandomTeleport実装

--- src/main/java/net/l_bulb/opmenu/StaffUtilitys/RandomTeleport.java
+++ src/main/java/net/l_bulb/opmenu/StaffUtilitys/RandomTeleport.java
@@ -1,48 +1,43 @@
 package net.l_bulb.opmenu.StaffUtilitys;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-public class RandomTeleport implements Listener{
+public class RandomTeleport implements Listener {
 
     @EventHandler
     public void randomTeleport(PlayerInteractEvent event) {
 	Player player = event.getPlayer();
 	if (!event.getPlayer().hasPermission("staff.tool")) {
-	    return;
-	}
-	if (event.getPlayer().getItemInHand().getType() == Material.RECORD_3 ) {
-	        Random r = new Random();
-	        int random = r.nextInt();
-	        
-	        }
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	}
-	    /*
-	}
-	    Random r = new Random();
-	          ArrayList<Player> players = new ArrayList<Player>();
-	          for(Player online : Bukkit.getServer().getOnlinePlayers()) {
-	          if(!(online == player)) {
 
-		          players.add(online);
-		          }
-		          int index = r.nextInt(players.size());
-		          Player loc = (Player) players.get(index);
-		          player.teleport(loc);
-		          player.sendMessage("§eTelporting you to " + "§6(name)" + "§e...");
-	          } return;
 	}
-	*/
+	if (player.getItemInHand().getType() == Material.RECORD_3) {
+        if (Bukkit.getOnlinePlayers().size() <= 1) {
+            player.sendMessage(ChatColor.RED + "Player is not found");
+        }
+        Random random = new Random();
+        List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
+        for(Player online : Bukkit.getServer().getOnlinePlayers()) {
+            if(Objects.equals(online, player)) {
+                continue;
+            }
+            int index = random.nextInt(players.size());
+            Player target = (Player) players.get(index);
+            player.teleport(target);
+            player.sendMessage(ChatColor.GREEN + "Telepored " + target.getDisplayName());
+            break;
+        }
+
+    }
+    }
 }
