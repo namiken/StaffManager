6de5a6afa5362e5c53a12aaf92a4b6923e48a0e5
fav <monmon25565@gmail.com>
2017-02-27 04:40:14 +0900

    クリエイティブ禁止 / staffModeに入るとFlyを許可

--- src/main/java/net/l_bulb/opmenu/Commands/StaffCommand.java
+++ src/main/java/net/l_bulb/opmenu/Commands/StaffCommand.java
@@ -1,43 +1,60 @@
 package net.l_bulb.opmenu.Commands;
 
+import java.util.Set;
+
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+import com.google.common.collect.Sets;
 
 import net.l_bulb.opmenu.StaffUtilitys.StaffItems;
-public class StaffCommand implements Listener,  CommandExecutor {
+
+public class StaffCommand implements Listener, CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args ) {
-	if (!sender.hasPermission("staff.staff")) {
-	}
-        if (sender instanceof Player) {
-	    Player player = (Player) sender;
-	    onEnableStaffMode(player);
-        } else {
-            sender.sendMessage("コンソールから実行できません。");
-        }
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+      if (!(sender instanceof Player)) {
+        sender.sendMessage("コンソールから実行できません。");
         return true;
+      }
+
+      Player player = (Player) sender;
+      toggleStaffMode(player);
+      return true;
+    }
+
+    private static Set<String> staffs = Sets.newHashSet();
+
+    public static void toggleStaffMode(Player player) {
+      if (!isInStaffMode(player)) {
+        onEnableStaffMode(player);
+      } else {
+        onDisableStaffMode(player);
+      }
     }
 
     public static void onEnableStaffMode(Player player) {
-	player.getInventory().clear();
-	StaffItems.addstaffItems(player);
-	player.sendMessage(ChatColor.GREEN + "[OpMenu] " + ChatColor.GOLD + "Staff Mode >> Enable!");
+      player.getInventory().clear();
+      StaffItems.addstaffItems(player);
+      player.sendMessage(ChatColor.GREEN + "[OpMenu] " + ChatColor.GOLD + "Staff Mode >> Enable!");
+      staffs.add(player.getUniqueId().toString());
+      player.setFlying(true);
+      player.setGameMode(GameMode.SURVIVAL);
     }
 
-    @EventHandler
-    public void rightClick(PlayerInteractEntityEvent event) { // Playerが左クリックした時
-
-	if(!event.getPlayer().hasPermission("staff.staff.event")){
-	    return;
-	}
-
+    public static void onDisableStaffMode(Player player) {
+      player.getInventory().clear();
+      player.sendMessage(ChatColor.RED + "[OpMenu] " + ChatColor.GOLD + "Staff Mode >> Disable!");
+      staffs.remove(player.getUniqueId().toString());
+      player.setFlying(false);
+      player.setGameMode(GameMode.SURVIVAL);
     }
 
-
+    public static boolean isInStaffMode(Player player) {
+      return staffs.contains(player.getUniqueId().toString());
+    }
 }
