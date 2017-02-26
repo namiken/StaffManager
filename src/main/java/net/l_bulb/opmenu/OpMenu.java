6dcd729ce252cdccb7ee8e9a727f757b03096977
fav <monmon25565@gmail.com>
2017-02-27 04:39:43 +0900

    メインクラスをわかりやすく

--- src/main/java/net/l_bulb/opmenu/OpMenu.java
+++ src/main/java/net/l_bulb/opmenu/OpMenu.java
@@ -8,6 +8,7 @@ import net.l_bulb.opmenu.Commands.MenuCommand;
 import net.l_bulb.opmenu.Commands.StaffCommand;
 import net.l_bulb.opmenu.Listener.MenuListener;
 import net.l_bulb.opmenu.StaffUtilitys.Compass;
+import net.l_bulb.opmenu.StaffUtilitys.FreezePlayer;
 import net.l_bulb.opmenu.StaffUtilitys.HealthCheck;
 import net.l_bulb.opmenu.StaffUtilitys.InventoryCheck;
 import net.l_bulb.opmenu.StaffUtilitys.RandomTeleport;
@@ -15,16 +16,25 @@ import net.l_bulb.opmenu.StaffUtilitys.RandomTeleport;
 public class OpMenu extends JavaPlugin {
     @Override
     public void onEnable() {
-        getCommand("staff").setExecutor(new StaffCommand());
-        getCommand("menu").setExecutor(new MenuCommand());
-        getCommand("fly").setExecutor(new FlyCommand());
-        getCommand("cc").setExecutor(new ClearChatCommand());
-        getServer().getPluginManager().registerEvents(new StaffCommand(), this);
-        getServer().getPluginManager().registerEvents(new MenuListener(), this);
-        getServer().getPluginManager().registerEvents(new Compass(), this);
-        getServer().getPluginManager().registerEvents(new HealthCheck(), this);
-        getServer().getPluginManager().registerEvents(new RandomTeleport(), this);
-        getServer().getPluginManager().registerEvents(new InventoryCheck(), this);
+	onCommands();
+	onEvents();
     }
 
+    public void onCommands() {
+	getCommand("staff").setExecutor(new StaffCommand());
+	getCommand("menu").setExecutor(new MenuCommand());
+	getCommand("fly").setExecutor(new FlyCommand());
+	getCommand("cc").setExecutor(new ClearChatCommand());
+    }
+
+    public void onEvents() {
+	getServer().getPluginManager().registerEvents(new StaffCommand(), this);
+	getServer().getPluginManager().registerEvents(new MenuListener(), this);
+	getServer().getPluginManager().registerEvents(new Compass(), this);
+	getServer().getPluginManager().registerEvents(new HealthCheck(), this);
+	getServer().getPluginManager().registerEvents(new RandomTeleport(), this);
+	getServer().getPluginManager().registerEvents(new InventoryCheck(), this);
+	getServer().getPluginManager().registerEvents(new EventCancel(), this);
+	getServer().getPluginManager().registerEvents(new FreezePlayer(), this);
+    }
 }
