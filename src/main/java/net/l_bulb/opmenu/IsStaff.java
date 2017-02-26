be13a4dfdfea30d2b1302d0facad95c68ac56aa5
fav <monmon25565@gmail.com>
2017-02-27 04:56:43 +0900

    StaffかStaffじゃないかをPermissionで判別するクラス追加

---
+++ src/main/java/net/l_bulb/opmenu/IsStaff.java
@@ -0,0 +1,14 @@
+package net.l_bulb.opmenu;
+
+import org.bukkit.entity.Player;
+
+public class IsStaff {
+
+    public static boolean isStaff(Player player) {
+	if (player.hasPermission("staff.staff")) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+}
