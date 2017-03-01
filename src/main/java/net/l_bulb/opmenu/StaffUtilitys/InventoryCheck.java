1a88755390bd9020762f68902ab6352eb6cdc2d8
fav <monmon25565@gmail.com>
2017-03-01 21:39:02 +0900

    Healthを追加

--- src/main/java/net/l_bulb/opmenu/StaffUtilitys/InventoryCheck.java
+++ src/main/java/net/l_bulb/opmenu/StaffUtilitys/InventoryCheck.java
@@ -8,6 +8,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class InventoryCheck implements Listener {
 
@@ -34,6 +35,10 @@ public class InventoryCheck implements Listener {
 	ItemStack leggins = target.getInventory().getLeggings();
 	ItemStack boots = target.getInventory().getBoots();
 	ItemStack[] items = target.getInventory().getContents();
+	ItemStack Health = new ItemStack(Material.SHEARS);
+
+	ItemMeta HealthMeta = Health.getItemMeta();
+	HealthMeta.setDisplayName(target.getDisplayName() + "has" + target.getHealth() + "Health");
 	Inventory inv = Bukkit.createInventory(player, 45, target.getDisplayName() + " Inventory!");
 	inv.setContents(items);
 	player.openInventory(inv);
@@ -41,6 +46,8 @@ public class InventoryCheck implements Listener {
 	inv.setItem(37, chestplate);
 	inv.setItem(38, leggins);
 	inv.setItem(39, boots);
+	inv.setItem(40, Health);
+
 	player.updateInventory();
     }
 }
