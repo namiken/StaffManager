304690eef8ca7de60124eaaad47fd9d5227ce7f6
fav <monmon25565@gmail.com>
2017-03-04 07:58:42 +0900

    Gitリポジトリ移動のため作業中のファイルをコミット

--- src/main/java/net/l_bulb/opmenu/OpMenu.java
+++ src/main/java/net/l_bulb/opmenu/OpMenu.java
@@ -1,16 +1,14 @@
 package net.l_bulb.opmenu;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.l_bulb.opmenu.Commands.ClearChatCommand;
 import net.l_bulb.opmenu.Commands.FlyCommand;
+import net.l_bulb.opmenu.Commands.FreezeCommand;
 import net.l_bulb.opmenu.Commands.MenuCommand;
 import net.l_bulb.opmenu.Commands.StaffCommand;
 import net.l_bulb.opmenu.Listener.MenuListener;
 import net.l_bulb.opmenu.StaffUtilitys.Compass;
-import net.l_bulb.opmenu.StaffUtilitys.FreezePlayer;
 import net.l_bulb.opmenu.StaffUtilitys.HealthCheck;
 import net.l_bulb.opmenu.StaffUtilitys.InventoryCheck;
 import net.l_bulb.opmenu.StaffUtilitys.RandomTeleport;
@@ -18,12 +16,6 @@ import net.l_bulb.opmenu.StaffUtilitys.RandomTeleport;
 public class OpMenu extends JavaPlugin {
     @Override
     public void onEnable() {
-	// 起動時Permissionを持っている人は全員Staff Mode Enableにする
-	Player player =  (Player) Bukkit.getServer().getOnlinePlayers();
-	if (IsStaff.isStaff(player)) {
-	    StaffCommand.onEnableStaffMode(player);
-	}
-
 	// registers
 	onCommandRegister();
 	onEventRegiser();
@@ -34,6 +26,7 @@ public class OpMenu extends JavaPlugin {
 	getCommand("menu").setExecutor(new MenuCommand());
 	getCommand("fly").setExecutor(new FlyCommand());
 	getCommand("cc").setExecutor(new ClearChatCommand());
+	getCommand("freeze").setExecutor(new FreezeCommand());
     }
 
     public void onEventRegiser() {
@@ -44,6 +37,5 @@ public class OpMenu extends JavaPlugin {
 	getServer().getPluginManager().registerEvents(new RandomTeleport(), this);
 	getServer().getPluginManager().registerEvents(new InventoryCheck(), this);
 	getServer().getPluginManager().registerEvents(new EventCancel(), this);
-	getServer().getPluginManager().registerEvents(new FreezePlayer(), this);
     }
 }
